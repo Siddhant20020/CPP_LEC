661bb726f2c92e1a7862f6d64121b810476acf46
Siddhant20020 <105770390+Siddhant20020@users.noreply.github.com>
2022-07-08 13:29:00 +0545

    Non Template argument

---
+++ .vscode/Chapter_10_Template/Need_for_template.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int find_max(int a, int b){
+    int result;
+    if (a>b)
+    result =a;
+    return result;
+}
+float find_max(float a, float b){
+    float result;
+    if (a>b)
+    result =a;
+    return result;
+}
+double find_max(double a, double b){
+    double result;
+    if (a>b)
+    result =a;
+    return result;
+}
+char find_max(char a, char b){
+    char result;
+    if (a>b)
+    result =a;
+    return result;
+}
+int main(){
+    int i1=15,i2=20;
+
+cout<<"Greater is "<<find_max(i1,i2)<<endl;
+float f1=40000.05,f2=38000.44;
+cout<<"Greater is "<<find_max(f1,f2)<<endl;
+double d1=55.05,d2=67.777;
+cout<<"Greater is "<<find_max(d1,d2)<<endl;
+char c1='a',c2='A';
+cout<<"Greater is "<<find_max(c1,c2)<<endl;
+return 0;
+}
