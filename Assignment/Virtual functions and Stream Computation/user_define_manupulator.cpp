2e97ceccfc66ccfef3625372bfd6ccf6b379a6e1
Siddhant20020 <105770390+Siddhant20020@users.noreply.github.com>
2022-07-20 11:45:14 +0545

    user defined manipulator

---
+++ Assignment/Virtual functions and Stream Computation/user_define_manupulator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+ostream& tb(ostream &os)
+{
+    os<<'\t'<<flush;
+    return os;
+}
+int main(){
+    int a=1,b=2,c=3,d=4;
+    cout<<a<<tb<<b<<tb<<c<<tb<<d<<endl;
+    return 0;
+}
