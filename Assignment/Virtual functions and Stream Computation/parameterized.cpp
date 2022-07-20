98a6af85deb3daf287b8cb1067f0dc91fda8a847
Siddhant20020 <105770390+Siddhant20020@users.noreply.github.com>
2022-07-20 11:53:15 +0545

    parametrized

---
+++ Assignment/Virtual functions and Stream Computation/parameterized.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+class TestManipulator{
+    private:
+    int n;
+    public:
+    TestManipulator (int num):n (num){}
+    friend ostream& operator<<(ostream&,TestManipulator);
+
+
+};
+ostream& operator<<(ostream& os, TestManipulator obj)
+{for (int i=0;i<obj.n;++i)
+os<<'\t';
+os<<flush;
+return os;
+}
+TestManipulator tb(int n)
+{
+    return TestManipulator(n);
+}
+int main(){
+    int a=1,b=1,c=3,d=4;
+    cout<<a<<tb(1)<<b<<tb(2)<<c<<tb(3)<<d<<endl;
+    return 0;
+}
